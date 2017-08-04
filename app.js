e2d7c92b53db12cae7a7ec36358048b4045d6a04
hchen <hchen229@gmail.com>
2017-08-03 21:29:39 -0700

    Remove access log rotator

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var FileStreamRotator = require('file-stream-rotator');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
@@ -21,29 +19,12 @@ var hbsHelpers = require('./utils/HBSHelpers');
 
 var app = express();
 
-if (conf.get("env") === "production") {
-    var logDirectory = __dirname + '/log';
-    // ensure log directory exists
-    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-    // create a rotating write stream
-    var accessLogStream = FileStreamRotator.getStream({
-        date_format: conf.get('log.dateformat'),
-        filename: logDirectory + '/access-%DATE%.log',
-        frequency: conf.get("log.frequency"),
-        verbose: false
-    });
-    app.use(logger('combined', {stream: accessLogStream}));
-} else {
-    app.use(logger('dev'));
-}
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 hbsHelpers.registerHelpers(hbs);
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser(conf.get('cookie.secret')));
